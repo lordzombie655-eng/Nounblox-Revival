6c57a7c9743e162083d4b64486c9f51f581e5bb5
lordzombie655-eng <lordzombie655@gmail.com>
2026-09-22 10:24:37 -0400

    Update db.php

--- core/db.php
+++ core/db.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'nounblx');
-define('DB_PASSWORD', '3myhnqpe');
-define('DB_NAME', 'nounblx');
+define('DB_SERVER', 'mysql-13f368e9-superwingmaster-a24f.b.aivencloud.com');
+define('DB_USERNAME', 'avnadmin');
+define('DB_PASSWORD', 'AVNS_IJ8Up7eRHfNFh0ln73i');
+define('DB_NAME', 'defaultdb');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
